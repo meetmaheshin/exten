4b67c93deb3994b15a3f3cb660ead3a1dc8b790d
meetmaheshin <meet.maheshin@gmail.com>
2026-05-04 14:06:45 +0530

    Bump version to 0.2.4

--- apps/extension/src/extension.ts
+++ apps/extension/src/extension.ts
@@ -273,7 +273,7 @@ export async function activate(context: vscode.ExtensionContext) {
   log("Ailancers Code extension activated");
 }
 
-const CURRENT_VERSION = "0.2.3";
+const CURRENT_VERSION = "0.2.4";
 
 async function checkForUpdates(apiClient: import("./services/ApiClient").ApiClient, log: (msg: string) => void): Promise<void> {
   try {
